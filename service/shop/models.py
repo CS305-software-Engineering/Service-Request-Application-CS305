4c3396be4bfa5948f65fb2d824f95c5af67f1fda
DhamiSamreet <56794225+DhamiSamreet@users.noreply.github.com>
2021-03-20 01:32:03 +0530

    Authentication under construction

--- service/shop/models.py
+++ service/shop/models.py
@@ -11,3 +11,11 @@ class EndUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     
+class serviceman(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    company_name = models.CharField(max_length=100,default="Freelancer")
+    is_plumber = models.BooleanField(default=False)
+    is_electrician = models.BooleanField(default=False)
+    is_mechanic = models.BooleanField(default=False)
+    other_services = models.CharField(max_length=100,default="None")
