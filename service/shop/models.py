735eb56c39adfa3e772ba9d888afc7dde87ccf32
Ujjwal-Yadav <69068363+Ujjwal-Yadav@users.noreply.github.com>
2021-03-25 17:13:07 +0530

    Update models.py

--- service/shop/models.py
+++ service/shop/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import RegexValidator
 
+
 # Model for End User
 class EndUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
