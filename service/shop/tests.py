be00897a0c42bac849684e7927f087203f034268
atul-77 <atulco1517@gmail.com>
2021-05-02 16:44:31 +0530

    testlogin

--- service/shop/tests.py
+++ service/shop/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from .models import *
 from django.utils import timezone
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 # from django.core.urlresolvers import reverse
 # from whatever.forms import WhateverForm
 # Create your tests here.
@@ -19,26 +22,56 @@ from django.utils import timezone
 #         self.assertEqual(phone,T.phone)
 
     
-class RequestTest(TestCase):
-    def create_request(self):
-        return Request.objects.create()
+# class RequestTest(TestCase):
+#     def create_request(self):
+#         return Request.objects.create()
 
-    def test_request_creation(self):
-        T = self.create_request()
-        self.assertTrue(isinstance(T,Request))
-        self.assertEqual(T.__str__(),T.requestid)
+#     def test_request_creation(self):
+#         T = self.create_request()
+#         self.assertTrue(isinstance(T,Request))
+#         self.assertEqual(T.__str__(),T.requestid)
 
 
 # Views Test
-class AddRequestTest(TestCase):
-    @classmethod
-    def setupdata(cls):
-        num_request = 17
-        for _ in range(num_request):
-            Request.objects.create()
+# class AddRequestTest(TestCase):
+#     @classmethod
+#     def setupdata(cls):
+#         num_request = 17
+#         for _ in range(num_request):
+#             Request.objects.create()
 
-    def test_get_status_response(self):
-        response = self.client.get('add_request')
-        self.assertTemplateUsed(response, 'shop/add_request.html')
-        # self.assertEqual(response.status_code, 200)
+#     def test_get_status_response(self):
+#         response = self.client.get('add_request')
+#         self.assertTemplateUsed(response, 'shop/add_request.html')
+#         # self.assertEqual(response.status_code, 200)
 
+class LoginTest(LiveServerTestCase):
+
+  def test(self):
+    selenium = webdriver.Chrome()
+    # selenium = webdriver.Firefox()
+    #Choose your url to visit
+    selenium.get('http://127.0.0.1:8000/login')
+    #find the elements you need to submit form
+    phone = selenium.find_element_by_name('phone')
+    password = selenium.find_element_by_name('password')
+    # player_name = selenium.find_element_by_id('id_name')
+    # player_height = selenium.find_element_by_id('id_height')
+    # player_team = selenium.find_element_by_id('id_team')
+    # player_ppg = selenium.find_element_by_id('id_ppg')
+
+    submit = selenium.find_element_by_name('loginsubmit')
+
+    #populate the form with data
+    phone.send_keys('7777777777')
+    password.send_keys('Service@123')
+    # player_name.send_keys('Lebron James')
+    # player_team.send_keys('Los Angeles Lakers')
+    # player_height.send_keys('6 feet 9 inches')
+    # player_ppg.send_keys('25.7')
+
+    #submit form
+    submit.send_keys(Keys.RETURN)
+
+    #check result; page source looks at entire html document
+    assert '7777777777' in selenium.page_source
